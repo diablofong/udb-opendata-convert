7a80f1fbb52c5b3d5bd81c4abaca88ed632ad11d
diablofong <diablofong@gmail.com>
2015-11-06 22:31:04 +0800

    完成讀取csv檔後,直接在終端機顯示內容

--- parse.js
+++ parse.js
@@ -1,12 +1,28 @@
 import fs from 'fs';
-import parsecsv from 'fast-csv';
 
-function * gen() {
-  console.log('start');
-  yield 'called';
-};
+import csvtojson from 'csvtojson';
 
-var g = gen();
-var v = g.next();
-console.log(123);
-console.log(v.value);
+var Converter = csvtojson.Converter
+
+var newConverter = new Converter({});
+
+var csvfile = 'source/RPT01-2015-11-05.csv';
+
+var saveFileName = 'output/csvtojson.json';
+
+fs.createReadStream(csvfile).pipe(newConverter);
+
+newConverter.on('end_parsed', function (jsonArray) {
+
+  // 可開啟這行在 Command Line 觀看 data 內容
+  console.log(jsonArray); //here is your result jsonarray 
+  
+  // 對 jsonArray 做處理，寫你的 code
+  
+  // 儲存成 JSON
+  // fs.writeFile 使用 File System 的 writeFile 方法做儲存
+  // 傳入三個參數（ 存檔名, 資料, 格式 ）
+  // fs.writeFile( saveFileName, JSON.stringify( jsonArray ), 'utf8');
+  console.log('csv to JSON done !!');
+
+});
